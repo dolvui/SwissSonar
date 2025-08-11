78f55dc0055e9a7d7c150077a429f1b1b2f9cec5
ghidalia <noa@ghidalia.fr>
2025-08-11 10:20:06 +0200

    requierment

--- swissUpdate.py
+++ swissUpdate.py
@@ -7,7 +7,7 @@ from CryptoToken import Token
 
 
 options = webdriver.ChromeOptions()
-options.add_argument('--headless')
+options.add_argument('--headless=new')
 options.add_argument('--edge-skip-compat-layer-relaunch')
 options.add_argument('--no-sandbox')
 driver = webdriver.Chrome(options=options)
