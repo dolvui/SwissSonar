8477786e8e5e947fd71e59c5f98df1599e096b08
ghidalia <noa@ghidalia.fr>
2025-08-11 16:04:27 +0200

    post: frontpage streamlit

--- main.py
+++ main.py
@@ -105,19 +105,8 @@ def frontpage():
         token = entity_to_token(e)
         tokens.append(token)
 
-    tokensScore = sort_token(tokens)
-    df_tokens = pd.DataFrame(tokensScore)
-
-    # if df_tokens is None or df_tokens.empty:
-    #     tokens, new_ids = swissUpdate.get_swissUpadte()
-    #
-    #     enriched_tokens = coingeckoAPI.fetch_market_data_fast(tokens, new_ids)
-    #
-    #     full_tokens = fetch_online_trend(enriched_tokens)
-    #
-    #     upsert_tokens_entry(full_tokens)
-    #
-    #     df_tokens = pd.DataFrame([t.dict_data() for t in full_tokens])
+    #tokensScore = sort_token(tokens)
+    df_tokens = pd.DataFrame([t.dict_data for t in tokens])
 
     # ========================
     # FRONT PAGE LAYOUT
