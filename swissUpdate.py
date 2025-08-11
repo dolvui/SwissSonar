a894c80c72017e6b81359893c9caf51d947a26c8
ghidalia <noa@ghidalia.fr>
2025-08-11 10:35:23 +0200

    requierment

--- swissUpdate.py
+++ swissUpdate.py
@@ -23,7 +23,7 @@ def init():
 
 def get_driver():
     chrome_options = Options()
-    chrome_options.binary_location = "/usr/bin/chromium-browser"
+    chrome_options.binary_location = "/usr/bin/chromium"
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
