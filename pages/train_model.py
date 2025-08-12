48ec655afa9bb7efb0a3b04b8030a4987997ba3e
ghidalia <noa@ghidalia.fr>
2025-08-12 15:52:36 +0200

    post: training page

--- pages/train_model.py
+++ pages/train_model.py
@@ -40,7 +40,7 @@ MODELS_DIR = Path("models")
 MODELS_DIR.mkdir(exist_ok=True)
 
 # Get list of model files
-model_files = sorted([f.name for f in MODELS_DIR.iterdir() if f.is_file()])
+model_files = sorted([f for f in MODELS_DIR.iterdir() if f.is_file()])
 
 st.subheader("List of available models:")
 
