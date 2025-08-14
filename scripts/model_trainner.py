419802bde224bebeee14916d25197f180e4c9990
ghidalia <noa@ghidalia.fr>
2025-08-14 15:19:08 +0200

    post: refactor logic + add workflow

--- scripts/model_trainner.py
+++ scripts/model_trainner.py
@@ -166,10 +166,11 @@ def run_model_and_plot(model_path, data, window=15, steps_ahead=50):
 def launch_train_model(id):
     from sqliteModels import fetch_models_by_id
     model_dict = fetch_models_by_id(id)
+    print(model_dict)
     model, losses, test_loss, norms = train_model(model_dict["name"],model_dict["days"], model_dict["windows"], model_dict["steps"], model_dict["epochs"], model_dict["lr"], model_dict["hidden"])
     model_path = f"models/tigerV2_{model_dict["name"]}.pt"
     norms_path = f"models/tigerV2_{model_dict["name"]}_norms.npy"
     torch.save(model.state_dict(), model_path)
     np.save(norms_path, norms)
-    files = [model_path, norms_path]
-    push_model_to_github(files, commit_msg=f"Add model {model_dict["name"]}-{id}-{datetime.datetime.now()}")
+    #files = [model_path, norms_path]
+    #push_model_to_github(files, commit_msg=f"Add model {model_dict["name"]}-{id}-{datetime.datetime.now()}")
