ea659486ac2bc0d6bd2d197dcc3ecaba5441dda3
ghidalia <noa@ghidalia.fr>
2025-08-21 11:17:03 +0200

    post: perso page

--- pages/personal_board.py
+++ pages/personal_board.py
@@ -3,8 +3,8 @@ import streamlit as st
 st.set_page_config(page_title="Personal Board", layout="wide", page_icon="👁️")
 st.title("Personal Board page")
 
-name = st.session_state['user']
-
-print(st.session_state['user'])
+# name = st.session_state['user']
+#
+# print(st.session_state['user'])
 print(st.session_state)
-st.write(f"{name}")
+#st.write(f"{name}")
